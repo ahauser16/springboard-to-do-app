dabfd0b5b3e2285952d2bf569ac65944c6db8fe5
Arthur Hauser <arthur.hauser@gmail.com>
2024-01-07 13:04:09 -0500

    add changes & notes from mentor session

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const taskList = document.querySelector('.taskList');
 
 document.addEventListener('DOMContentLoaded', localStorageRetrievalOnStartup);
 
+
+// window.onload = (event) => {
+//   NB it used to matter whether you used "onload" or "DOMContentLoaded" but now browsers are more consistent in their implementation of these events.
+
+//step one bind all elements within the INITIALIZE FUNCTION
+//NB--> do not name functions to include "saveLocalStorage"...use a more generic name.  the variables should be more precise but later when we get to OOP we will use more generic names for the variables as well.  majority of companies don't use OOP on the front end but the JQUERY, REACT, and other frameworks are all OOP.  so we will learn OOP in the next module.  for now, we are just learning the basics of JS and DOM manipulation.  trying to encourage Composition Over Inheritance model. 
+
+//step two...makeTodoItem() function is  cyclomatic complexity of 2.  it is doing too much.  it is creating the elements, adding the event listeners, and adding the elements to the DOM.  we need to break this function up into smaller functions.  we will create a function called "createTodoItem()" and then call that function from within the makeTodoItem() function.  we will also create a function called "addEventListeners()" and call that function from within the makeTodoItem() function.  we will also create a function called "addTodoItemToDOM()" and call that function from within the makeTodoItem() function.  we will also create a function called "saveToLocalStorage()" and call that function from within the makeTodoItem() function.  we will also create a function called "updateLocalStorageAfterDeletion()" and call that function from within the makeTodoItem() function.  we will also create a function called "updateTaskStatusInLocalStorage()" and call that function from within the makeTodoItem() function.  try to make the bigger functions into smaller functions.
+
+//refactor step three--> js doc follows a syntax to create documentation for your code.  it is a way to document your code.  it is a way to document your code so that other developers can understand your code.  it is a way to document your code so that you can understand your code.  it is a way to document your code so that you can understand your code in the future.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing.  it is a way to document your code so that you can understand your code in the future when you have forgotten what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing and you are working on a different project and you need to come back to this project and understand what you were doing.
+
+
+// };
+
+
+
+
 function saveToLocalStorage(id, taskName, taskDesc, isCompleted) {
     try {
         let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -21,9 +38,6 @@ function saveToLocalStorage(id, taskName, taskDesc, isCompleted) {
         console.error("Error in saveToLocalStorage:", error);
     }
 }
-
-
-
 
 taskList.addEventListener('click', function (e) {
     if (e.target.classList.contains('delete-task-btn')) {
@@ -47,22 +61,22 @@ form.addEventListener('submit', function (e) {
     taskDescInput.value = '';
 });
 
-function saveToLocalStorage(id, taskName, taskDesc, isCompleted) {
-    try {
-        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        const newTask = {
-            id: new Date().getTime(),
-            taskName: taskName,
-            taskDescription: taskDesc,
-            isCompleted: false  
-        };
-        console.log("Saving new task:", newTask); 
-        tasks.push(newTask);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    } catch (error) {
-        console.error("Error in saveToLocalStorage:", error);
-    }
-}
+// function saveToLocalStorage(id, taskName, taskDesc, isCompleted) {
+//     try {
+//         let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+//         const newTask = {
+//             id: new Date().getTime(),
+//             taskName: taskName,
+//             taskDescription: taskDesc,
+//             isCompleted: false  
+//         };
+//         console.log("Saving new task:", newTask); 
+//         tasks.push(newTask);
+//         localStorage.setItem('tasks', JSON.stringify(tasks));
+//     } catch (error) {
+//         console.error("Error in saveToLocalStorage:", error);
+//     }
+// }
 
 
 function makeTodoItem(id, name, description, isCompleted = false) {
